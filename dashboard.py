b34e4a203985224900de5735c7e9427443c78c89
yaoqi <yaoqi@umich.edu>
2022-04-09 22:57:04 -0400

    update code

--- dashboard.py
+++ dashboard.py
@@ -181,12 +181,6 @@ viz1_col1, viz1_col2 = st.columns([2, 6])
 race = list(age_group['Race'].unique())
 race.insert(0, 'Not Selected')
 
-# age = list(age_group['AgeCategory'].unique())
-# age.insert(0, 'Not Selected')
-
-# gender = list(age_group['Sex'].unique())
-# gender.insert(0, 'Not Selected')
-
 with viz1_col1:
     race_selections = st.selectbox ("Race", race)
     # age_selections = st.selectbox("Age", age)
@@ -734,3 +728,115 @@ with viz3_col2:
                     patients with heart disease had about 2.1 times more skin cancer.</p>"""
         st.markdown(kidney_desc, unsafe_allow_html=True)
         st.write(skin_viz)
+
+####################
+##### Sidebar  #####
+####################
+def user_select_features():
+    race = st.sidebar.selectbox("Race", options=(race for race in df['Race'].unique()))
+    sex = st.sidebar.selectbox("Sex", options=(sex for sex in df['Sex'].unique()))
+    age_cat = st.sidebar.selectbox("Age category", options=(age for age in df['AgeCategory'].unique()))
+    bmi_cat = st.sidebar.number_input('Enter your BMI: ', 12, 95)
+    sleep_time = st.sidebar.number_input("How many hours on average do you sleep?", 0, 24, 7)
+    gen_health = st.sidebar.selectbox("How can you define your general health?",
+                                        options=(health for health in df['GenHealth'].unique()))
+    phys_health = st.sidebar.number_input("For how many days during the past 30 days was"
+                                            " your physical health not good?", 0, 30, 0)
+    ment_health = st.sidebar.number_input("For how many days during the past 30 days was"
+                                            " your mental health not good?", 0, 30, 0)
+    phys_act = st.sidebar.selectbox("Have you played any sports (running, biking, etc.)"
+                                    " in the past month?", options=("No", "Yes"))
+    smoking = st.sidebar.selectbox("Have you smoked at least 100 cigarettes in"
+                                    " your entire life (approx. 5 packs)?)",
+                                    options=("No", "Yes"))
+    alcohol_drink = st.sidebar.selectbox("Do you have more than 14 drinks of alcohol (men)"
+                                            " or more than 7 (women) in a week?", options=("No", "Yes"))
+    stroke = st.sidebar.selectbox("Did you have a stroke?", options=("No", "Yes"))
+    diff_walk = st.sidebar.selectbox("Do you have serious difficulty walking"
+                                        " or climbing stairs?", options=("No", "Yes"))
+    diabetic = st.sidebar.selectbox("Have you ever had diabetes?",
+                                    options=(diabetic for diabetic in df['Diabetic'].unique()))
+    asthma = st.sidebar.selectbox("Do you have asthma?", options=("No", "Yes"))
+    kid_dis = st.sidebar.selectbox("Do you have kidney disease?", options=("No", "Yes"))
+    skin_canc = st.sidebar.selectbox("Do you have skin cancer?", options=("No", "Yes"))
+
+    features = pd.DataFrame({
+                "PhysicalHealth": [phys_health],
+                "MentalHealth": [ment_health],
+                "SleepTime": [sleep_time],
+                "BMI": [bmi_cat],
+                "Smoking": [smoking],
+                "AlcoholDrinking": [alcohol_drink],
+                "Stroke": [stroke],
+                "DiffWalking": [diff_walk],
+                "Sex": [sex],
+                "AgeCategory": [age_cat],
+                "Race": [race],
+                "Diabetic": [diabetic],
+                "PhysicalActivity": [phys_act],
+                "GenHealth": [gen_health],
+                "Asthma": [asthma],
+                "KidneyDisease": [kid_dis],
+                "SkinCancer": [skin_canc]
+            })
+    return features
+
+st.sidebar.title("Enter Your Personal Statistics")
+st.sidebar.image("images/heart_side.jpeg", width=100)
+input_df = user_select_features()
+submit = st.sidebar.button("Predict")
+
+####################
+##### Predict  #####
+####################
+from sklearn.model_selection import train_test_split
+from imblearn.over_sampling import SMOTE
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+
+X_train, x_test, y_train, y_test = train_test_split(df.drop('HeartDisease',axis=1),
+                                                    df['HeartDisease'], test_size=0.30,
+                                                    random_state=101)
+
+num_pip = Pipeline([
+        ("scaler", StandardScaler())
+    ])
+categorical_pip = Pipeline([("cat_encoder", OneHotEncoder(sparse=False))])
+categorical_attr = ["Smoking", "AlcoholDrinking", "Stroke", "DiffWalking", 
+               "Sex", "AgeCategory", "Race", "Diabetic", "PhysicalActivity", 
+               "GenHealth", "Asthma", "KidneyDisease", "SkinCancer"]
+num_attr = ["BMI", "PhysicalHealth", "MentalHealth", "SleepTime"]
+
+preprocess_pipeline = ColumnTransformer([
+        ("num", num_pip, num_attr),
+        ("cat", categorical_pip, categorical_attr)
+    ])
+
+X = preprocess_pipeline.fit_transform(
+    X_train[num_attr + categorical_attr])
+
+y = y_train
+
+oversample = SMOTE()
+X_oversample, y_oversample = oversample.fit_resample(X, y)
+logmodel = LogisticRegression()
+logmodel.fit(X_oversample, y_oversample)
+
+if submit:
+    trans_input_df = preprocess_pipeline.transform(input_df[num_attr + categorical_attr])
+    prediction = logmodel.predict(trans_input_df)
+    prediction_prob = logmodel.predict_proba(trans_input_df[num_attr + categorical_attr])
+    if prediction == 0:
+        st.markdown(f"**The probability that you'll have"
+                    f" heart disease is {round(prediction_prob[0][1] * 100, 2)}%."
+                    f" You are healthy!**")
+    else:
+        st.markdown(f"**The probability that you will have"
+                    f" heart disease is {round(prediction_prob[0][1] * 100, 2)}%."
+                    f" It sounds like you are not healthy.**")
+
+# pred_col1, viz3_col2 = st.columns([3, 6])
